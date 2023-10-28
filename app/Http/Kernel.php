c370caa7c17a293010f494e81c78c2cd30f91b51
Faizal_Husain_Adiasha <adihusain0@gmail.com>
2023-10-29 01:40:40 +0700

    feat : add peminjaman

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -53,6 +53,8 @@ class Kernel extends HttpKernel
      * @var array<string, class-string|string>
      */
     protected $routeMiddleware = [
+        'petugas' => \App\Http\Middleware\AuthPetugasMiddleware::class,
+        'anggota' => \App\Http\Middleware\AuthAnggotaMiddleware::class,
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'auth.session' => \Illuminate\Session\Middleware\AuthenticateSession::class,
