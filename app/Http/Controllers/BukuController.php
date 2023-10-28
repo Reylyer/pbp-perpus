f41e4a424b2bf8f3b817f1fbdad3c67c9f4f25cc
ZZIDZZ <zidanutomo@gmail.com>
2023-10-28 23:40:40 +0700

    test

--- app/Http/Controllers/BukuController.php
+++ app/Http/Controllers/BukuController.php
@@ -10,19 +10,30 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Auth;
 
 class BukuController extends Controller
 {
     function list(Request $request)
     {
         $books = DB::select(
-            "SELECT b.idbuku as idbuku, b.isbn as isbn, b.judul as judul, k.nama as nama_kategori, b.pengarang as pengarang, b.penerbit as penerbit, YEAR(b.tgl_insert) as tahun
-            FROM buku b JOIN kategori k ON b.idkategori = k.idkategori
+            "SELECT
+                b.idbuku as idbuku,
+                b.isbn as isbn,
+                b.judul as judul,
+                k.nama as nama_kategori,
+                b.pengarang as pengarang,
+                b.penerbit as penerbit,
+                b.tahun_terbit as tahun
+            FROM buku b
+            JOIN kategori k
+            ON b.idkategori = k.idkategori
             WHERE isbn LIKE '%$request->s%'
             OR b.isbn LIKE '%$request->s%'
             OR k.nama LIKE '%$request->s%'
             OR b.pengarang LIKE '%$request->s%'
             OR b.penerbit LIKE '%$request->s%'
+            OR b.tahun_terbit LIKE '%$request->s%'
             ",
         );
 
@@ -44,8 +55,10 @@ class BukuController extends Controller
                     b.file_gambar as file_gambar,
                     b.stok as stok,
                     b.stok_tersedia as stok_tersedia,
-                    YEAR(b.tgl_insert) as tahun
-            FROM buku b JOIN kategori k ON b.idkategori = k.idkategori
+                    b.tahun_terbit as tahun
+            FROM buku b
+            JOIN kategori k
+            ON b.idkategori = k.idkategori
             WHERE b.idbuku = ?",
             [$idbuku]
         );
@@ -58,7 +71,7 @@ class BukuController extends Controller
                               FROM rating_buku
                               WHERE idbuku = '$idbuku'
                               ");
-        
+
         $rating = $rating ? number_format($rating[0]->rating, 1) : null;
 
         return view('buku.show', ['book' => $book[0], 'komentar' => $komentar, 'rating' => $rating]);
@@ -69,44 +82,8 @@ class BukuController extends Controller
         return Buku::search($request->s)->get();
     }
 
-    function create(){
-        $kategori_pair = Kategori::all('idkategori', 'nama');
-        return view('buku.create', ['kategori_pair' => $kategori_pair]);
-    }
-
-    function doCreate(Request $request){
-        $validated = $request->validate([
-            'isbn'        => 'required|string|unique:buku',
-            'judul'       => 'required|string',
-            'idkategori'  => 'required',
-            'pengarang'   => 'required|string',
-            'penerbit'    => 'required|string',
-            'kota_terbit' => 'required|string',
-            'editor'      => 'required|string',
-            'file_gambar' => 'image|mimes:jpeg,png,jpg,gif,svg|max:2048',
-            'stok'        => 'nullable|numeric',
-        ]);
-
-        if ($validated['file_gambar'] !== null) {
-            $validated['file_gambar'] = Storage::disk('public')->put('images', $validated['file_gambar']);
-            error_log($validated['file_gambar']);
-        }
-
-        $validated['stok_tersedia'] = $validated['stok'];
-
-        $buku = Buku::create($validated);
-        error_log($buku);
-        $buku->save();
-
-        // $create = DB::insert('insert into buku (isbn, judul, idkategori, pengarang, kota_terbit, editor, file_gambar, stok) values (?)',
-        //                     [$isbn, $judul, $idkategori, $pengarang, $penerbit, $editor, $file_gambar]);
-
-        return redirect()->route('buku.list');
-
-    }
-
     function komentar(Request $request, $idbuku){
-        $anggota = $request->session()->get('anggota');
+        $anggota = Auth::guard('anggota')->user();
 
         if ($request->input('komentar') !== null) {
             $komentar = KomentarBuku::create([
@@ -121,7 +98,7 @@ class BukuController extends Controller
     }
 
     function rating(Request $request, $idbuku){
-        $anggota = $request->session()->get('anggota');
+        $anggota = Auth::guard('anggota')->user();
 
         $isAlreadyRated = DB::select("SELECT * FROM rating_buku WHERE idbuku = '$idbuku' AND noktp = '$anggota->noktp'");
 
